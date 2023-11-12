114684a5f3edfbbda3a575d9a596bc31cceee0f0
“rakibul8226” <rakibul8226@gmail.com>
2023-11-13 02:09:05 +0600

    active navigate bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import '../../css/mycss.css'
 const Header = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/user">User</Link>
-            <Link to="/post">Post</Link>
-            <Link to="/about">About us</Link>
-            <Link to="/contact">Contact us</Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/user">User</NavLink>
+            <NavLink to="/post">Post</NavLink>
+            <NavLink to="/about">About us</NavLink>
+            <NavLink to="/contact">Contact us</NavLink>
+            
         </nav>
     );
 };
